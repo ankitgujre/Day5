984d8c822238724948891b87a62e15faf164229f
ankitgujre <ankitgujre374@gmail.com>
2025-10-30 22:21:25 +0530

    comparision operators

--- script.js
+++ script.js
@@ -33,4 +33,21 @@ console.log(a)
 
 // Comparision Operator
 
-// 
+// ==, ===, !=, !==, >,<, <=, >=
+
+// == not strict compare  ise kabhi bhi use nahi karna
+// === strict compare   ise karna hai
+
+// 12 == "12"  yeh true return karega
+console.log(12=='12')
+console.log(12==='12')   //false
+
+// != never use this
+// !== always use this
+console.log(12!='12')  // false
+console.log(12!=='12')  // true
+
+console.log(12>13)
+console.log(12<13)
+console.log(12<=13)
+console.log(12<=12)
