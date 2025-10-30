ad26eecebb3a65136ce2a831eb699eb3f5fec6f7
ankitgujre <ankitgujre374@gmail.com>
2025-10-30 22:09:42 +0530

    operators

---
+++ script.js
@@ -0,0 +1,36 @@
+// Arithmetic Operator
+
+// + - * / % **
+
+console.log(12+13)
+console.log(12-13)
+console.log(12*13)
+console.log(12/13)
+console.log(12%13)
+console.log(12**13)
+console.log(2**3)
+
+// Assignment Operator
+
+// =, +=, -=, *=, /=, %=
+
+let a = 12;  
+
+a += 13;  // particular value se badhana 25
+console.log(a)
+
+a -= 13;
+console.log(a)
+
+a *= 13;
+console.log(a)
+
+a /= 13;
+console.log(a)
+
+a %= 3;
+console.log(a)
+
+// Comparision Operator
+
+// 
