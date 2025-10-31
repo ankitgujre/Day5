b0d8e9dfa67c3fcf2407c68d3b29722874cd6bc9
ankitgujre <ankitgujre374@gmail.com>
2025-10-31 15:02:14 +0530

    questions

---
+++ assifn.js
@@ -0,0 +1,35 @@
+/**-----------------Basic Operator---------- */
+
+let a = 10; b = 20;
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a**b)
+console.log(a/b)
+console.log(a%b)
+
+
+let x = 5;
+// x = x+3;  8
+// x-=3;
+// x+=3;
+// x*=3;
+x/=3;
+console.log(x)
+
+let count = 5;
+console.log(count)
+count++;
+console.log(count);
+
+count = count++ + 3;
+console.log(count)
+
+/**------------------Compare----------- */
+
+let num = 5;
+let n = "5";
+
+console.log(num === n)
+console.log(num == n)
--- script.js
+++ script.js
@@ -51,3 +51,62 @@ console.log(12>13)
 console.log(12<13)
 console.log(12<=13)
 console.log(12<=12)
+
+
+// Logical Operstors
+
+// && || !   ye true aur false return karte hai
+
+// &&  isme agar dono condition true hai toh true return karega koi ek false hai toh false
+// return karega dono condition ka true hona jaruri hai
+
+/*
+|| = isme koi ek condition true ho toh true aur dono true hai toh bhi true baki me false
+*/
+
+// ! not ye true ko false flase ko true kardega
+
+
+/*          Ternary Operator*/
+
+// condition ? doThis:doThat
+
+12>13 ? console.log("T"):console.log("F")
+12<13 ? console.log("T"):console.log("F")
+
+/*-----------Type checking operators* */
+
+// typeof
+// instaceof   aage padege
+
+console.log(typeof 12)
+
+
+
+/**----------------String Operator*------------ */
+
+// + for concatenation
+
+console.log("Ankit"+"Gujre")
+console.log("Ankit"+" "+"Gujre")
+
+
+
+
+
+
+/*--------------------spread and rest operators---------*/
+
+//  spread operator ... kehlata hai
+// let arr = [1,2,3,4]
+// a = [...arr]    copy karne ke liye
+// rest operator bhi ... kehlata hai  functions ke saath use hota hai
+
+/**-------------nullish coelshing operator ----------*/
+
+/**-------optional chaning----- */
+
+/**-------------variable hoisting in js----------- */
+
+// var b = 12;  hoisting ka matlab hai variable ko banane se pehle bhi use kiya ja sakta hai
+// hoisting me apka variable toot jata hai do hisso me
